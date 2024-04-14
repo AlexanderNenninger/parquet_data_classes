4a7791da3265f01d2e20da47d02c0d312c1a6bef
Alexander Nenninger <alexander.nenninger@nttdata.com>
2024-04-14 12:24:36 +0200

    add tests

---
+++ test_custom_dataset_to_parqet.py
@@ -0,0 +1,109 @@
+import warnings
+from math import inf, nan
+
+import polars as pl
+import polars.testing
+import pytest
+
+from custom_dataset_to_parquet import ConversionWarning, DataSet, MetaData
+
+
+@pytest.fixture()
+def dataframe():
+    return pl.DataFrame(
+        {
+            "partition": ["1", "1", "2", "2"],
+            "lexical_partition": ["1", "1", "2", "2"],
+            "string_partition": ["1", "2", "1", "2"],
+            "int_partition": [1, 2, 1, 2],
+            "integer": [1, 2, 3, None],
+            "float": [4.0, None, nan, inf],
+            "string": ["d", "e", "f", None],
+            "categorical": ["a", "b", "a", None],
+            "list": [[1, 2], [3, None, 5], None, [7, 8, 9]],
+            "datetime": ["2024-01-01", "2024-02-29T16:32", None, None],
+        },
+        schema_overrides={
+            "partition": pl.Categorical(ordering="physical"),
+            "lexical_partition": pl.Categorical(ordering="lexical"),
+            "categorical": pl.Categorical(ordering="physical"),
+            "list": pl.List(pl.Int32),
+        },
+    ).with_columns(
+        pl.col("datetime").str.strptime(
+            dtype=pl.Datetime(time_unit="ms", time_zone="Europe/Berlin"),
+        )
+    )
+
+
+@pytest.fixture
+def metadata():
+    return MetaData()
+
+
+@pytest.fixture
+def dataset(metadata, dataframe):
+    return DataSet(metadata, dataframe)
+
+
+def test_write_read_without_partitioning(dataset, tmp_path):
+    dataset.write_parquet(tmp_path / "no-partition.parquet")
+    deserialized = DataSet.read_parquet(tmp_path / "no-partition.parquet")
+    dataset.assert_eq(deserialized)
+
+
+def test_write_read_with_partitioning(dataset, tmp_path):
+    # No warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        dataset.write_parquet(
+            tmp_path / "partition_no_warning", partition_cols=["partition"]
+        )
+        deserialized = DataSet.read_parquet(tmp_path / "partition_no_warning")
+        dataset.assert_eq(deserialized, check_column_order=False)
+
+
+def test_lexical_partition(dataset, tmp_path):
+    with pytest.warns(ConversionWarning):
+        partion_col = "lexical_partition"
+        dataset.write_parquet(tmp_path / partion_col, partition_cols=[partion_col])
+        deserialized = DataSet.read_parquet(tmp_path / partion_col)
+        assert dataset.dataframe[partion_col].dtype.is_(pl.Categorical("lexical"))
+        assert deserialized.dataframe[partion_col].dtype.is_(pl.Categorical("physical"))
+        polars.testing.assert_frame_equal(
+            dataset.dataframe[[partion_col, "integer"]],
+            deserialized.dataframe[[partion_col, "integer"]],
+            check_row_order=False,
+            check_column_order=False,
+            check_dtype=False,
+        )
+
+
+def test_string_partition(dataset, tmp_path):
+    with pytest.warns(ConversionWarning):
+        partion_col = "string_partition"
+        dataset.write_parquet(tmp_path / partion_col, partition_cols=[partion_col])
+        deserialized = DataSet.read_parquet(tmp_path / partion_col)
+        polars.testing.assert_frame_equal(
+            dataset.dataframe[[partion_col, "integer"]],
+            deserialized.dataframe[[partion_col, "integer"]],
+            check_row_order=False,
+            check_column_order=False,
+            check_dtype=False,
+        )
+
+
+def test_int_partition(dataset, tmp_path):
+    with pytest.warns(ConversionWarning):
+        partion_col = "int_partition"
+        dataset.write_parquet(tmp_path / partion_col, partition_cols=[partion_col])
+        deserialized = DataSet.read_parquet(tmp_path / partion_col)
+        polars.testing.assert_frame_equal(
+            dataset.dataframe[[partion_col, "integer"]],
+            deserialized.dataframe.select(
+                pl.col(partion_col).cast(pl.Int64), "integer"
+            ),
+            check_row_order=False,
+            check_column_order=False,
+            check_dtype=False,
+        )
